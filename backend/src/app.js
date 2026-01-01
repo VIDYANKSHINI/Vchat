85a8dcd4818010a56ca017a0213030d661547a4f
VIDYANKSHINI <vibhutevidyankshini8@gmail.com>
2026-01-01 22:34:59 +0530

    Set up Express server with MongoDB and Socket.IO

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import { createServer } from 'node:http';
 
-import { Server } from 'socket.io';
+import { Server } from "socket.io";
 
 import mongoose from 'mongoose';
-
+import connectToSocket from './controllers/socketManager.js';
 
 import cors from 'cors';
 
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000));
 
 
 app.get("/home", (req, res) => {
@@ -17,8 +22,12 @@ app.get("/home", (req, res) => {
 });
 
 const start = async () => {
+app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://vibhutevidyankshini8:lbsnaadream8@cluster0.ot54md2.mongodb.net/?appName=Cluster0")
 
-    app.listen(8000,() => {
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+
+    server.listen(app.get ("port"),() => {
         console.log("LISTENIN ON PORT 8000");
     });
 
---
+++ backend/src/controllers/socketManager.js
@@ -0,0 +1,10 @@
+
+
+
+const connectToSocket = (server) => {
+    const io = new Server ( server );
+
+    return io;
+}
+
+export default connectToSocket;
